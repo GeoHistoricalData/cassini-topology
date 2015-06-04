2229805464ba0dbcbfaf179196699307a10f27df
Julien Perret <julien.perret@gmail.com>
2015-06-04 13:08:37 +0200

    added node and edge table names as parameters for connected components

--- connected_components.sql
+++ connected_components.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE FUNCTION iterative_depth_first_search(atopology varchar, initial int, component_id int)
+﻿CREATE OR REPLACE FUNCTION iterative_depth_first_search(atopology varchar, anode varchar, anedge varchar, initial int, component_id int)
 RETURNS VOID AS $$
 DECLARE
   v integer;
@@ -18,18 +18,18 @@ BEGIN
     --RAISE NOTICE 'v = %', v;
     -- if v is not labeled as discovered:
     -- SELECT component INTO comp FROM export.node WHERE node_id = v;
-    EXECUTE format('SELECT component FROM %I.node WHERE node_id =  = $1', atopology) INTO comp USING v;
+    EXECUTE format('SELECT component FROM %I.%I WHERE node_id =  = $1', atopology, anode) INTO comp USING v;
     IF comp IS NULL THEN
       -- label v as discovered
-      EXECUTE format('UPDATE %I.node SET component = $1 WHERE node_id = $2', atopology) USING component_id, v;
+      EXECUTE format('UPDATE %I.%I SET component = $1 WHERE node_id = $2', atopology, anode) USING component_id, v;
       -- for all edges from v to w in G.adjacentEdges(v) do S.push(w)
       --INSERT INTO stack SELECT distinct(new_id) FROM 
       --  (SELECT start_node AS new_id FROM export.edge WHERE end_node = v UNION SELECT end_node AS new_id FROM export.edge WHERE start_node = v) AS tmp
       --  WHERE NOT EXISTS (SELECT 1 FROM stack WHERE id = new_id);
       EXECUTE format('INSERT INTO stack SELECT distinct(new_id) FROM '
-        || '(SELECT start_node AS new_id FROM %I.edge WHERE end_node = $1 UNION SELECT end_node AS new_id FROM %I.edge WHERE start_node = $1) AS tmp'
+        || '(SELECT start_node AS new_id FROM %I.%I WHERE end_node = $1 UNION SELECT end_node AS new_id FROM %I.%I WHERE start_node = $1) AS tmp'
         || 'WHERE NOT EXISTS (SELECT 1 FROM stack WHERE id = new_id)',
-        atopology) USING v;
+        atopology, anedge, atopology, anedge) USING v;
       SELECT count(*) INTO v FROM stack;
       --RAISE NOTICE '   stack = %', v;
     END IF;
@@ -41,7 +41,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION connected_components(atopology varchar)
+CREATE OR REPLACE FUNCTION connected_components(atopology varchar, anode varchar, anedge varchar)
 RETURNS VOID AS $$
 DECLARE
   id integer;
@@ -50,12 +50,12 @@ BEGIN
   comp := 1;
   LOOP
     --SELECT node_id INTO id FROM export.node WHERE component IS NULL LIMIT 1;
-    EXECUTE format('SELECT node_id FROM %I.node WHERE component IS NULL LIMIT 1', atopology) INTO id;
+    EXECUTE format('SELECT node_id FROM %I.%I WHERE component IS NULL LIMIT 1', atopology, anode) INTO id;
     IF id IS NULL THEN
       EXIT;
     END IF;
     RAISE NOTICE 'connected_components %', comp;
-    EXECUTE format('SELECT iterative_depth_first_search(%L, $1, $2)', atopology) USING id, comp;
+    EXECUTE format('SELECT iterative_depth_first_search(%L, %L, %L, $1, $2)', atopology, anode, anedge) USING id, comp;
     comp := comp + 1;
   END LOOP;
 END;
