26a109b5c147b89ce9e1a3a55de412090b256f6a
Julien Perret <julien.perret@gmail.com>
2015-06-10 17:27:06 +0200

    added columns to connected_component table to classify them

--- compute_connected_components.sql
+++ compute_connected_components.sql
@@ -6,7 +6,7 @@
 ALTER TABLE france_cassini_routes_topo.cassini_node ADD COLUMN component integer DEFAULT NULL;
 
 -- compute the connected components
-SELECT connected_components('france_cassini_routes_topo', 'cassini_node', 'cassini_edge');
+SELECT connected_components('france_cassini_routes_topo', 'cassini_node', 'cassini_edge', 'component');
 
 -- drop component column
 --ALTER TABLE france_cassini_routes_topo.cassini_edge DROP COLUMN component;
@@ -23,9 +23,41 @@ CREATE TABLE france_cassini_routes_topo.connected_component AS
   FROM france_cassini_routes_topo.cassini_edge
   GROUP BY component;
 
+-- compute the geometry of the connected components (union of its components geometries)
 SELECT AddGeometryColumn ('france_cassini_routes_topo','connected_component','geom',2154,'GEOMETRY',2);
 UPDATE france_cassini_routes_topo.connected_component c SET geom = (SELECT ST_Union(geom) FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id);
 
+-- add indices
 CREATE INDEX connected_component_index ON france_cassini_routes_topo.connected_component USING gist(geom);
 CREATE INDEX connected_component_index_pk ON france_cassini_routes_topo.connected_component USING btree(component_id);
 GRANT SELECT ON TABLE france_cassini_routes_topo.connected_component TO ghdb_user;
+
+-- add new component column
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN red double precision DEFAULT 0;
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN white double precision DEFAULT 0;
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN trail double precision DEFAULT 0;
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN forest double precision DEFAULT 0;
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN ferry double precision DEFAULT 0;
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN gap double precision DEFAULT 0;
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN bridge double precision DEFAULT 0;
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN fictive double precision DEFAULT 0;
+
+UPDATE france_cassini_routes_topo.connected_component c SET
+	red = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='red'), 0),
+	white = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='white'), 0),
+	trail = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='trail'), 0),
+	forest = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='forest'), 0),
+	ferry = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='ferry'), 0),
+	gap = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='gap'), 0),
+	bridge = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='bridge'), 0),
+	fictive = coalesce((SELECT sum(ST_Length(geom))/c.length FROM france_cassini_routes_topo.cassini_edge e WHERE e.component = c.component_id AND e.road_type='fictive'), 0);
+
+ALTER TABLE france_cassini_routes_topo.connected_component ADD COLUMN component_type varchar DEFAULT 'mixed';
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'red' WHERE red > 0.75;
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'white' WHERE white > 0.75;
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'trail' WHERE trail > 0.75;
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'forest' WHERE forest > 0.75;
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'ferry' WHERE ferry > 0.75;
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'gap' WHERE gap > 0.75;
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'bridge' WHERE bridge > 0.75;
+UPDATE france_cassini_routes_topo.connected_component c SET component_type = 'fictive' WHERE fictive > 0.75;
