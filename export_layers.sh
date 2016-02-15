37cc29189c1eaf3e588cff40673209ec1be20e94
Julien Perret <julien.perret@gmail.com>
2016-02-15 10:16:16 +0100

    adding tgz step

--- export_layers.sh
+++ export_layers.sh
@@ -3,7 +3,7 @@
 #echo "$# parameters"
 
 if [ $# != 7 ]; then
-    echo "You need to provide 6 parameters: directory, host, port, db, schema, user, password"
+    echo "You need to provide 7 parameters: directory, host, port, db, schema, user, password"
     exit 1
 fi
 
@@ -19,3 +19,13 @@ ogr2ogr -a_srs EPSG:2154 $1 PG:"host='$2' port='$3' user='$6' dbname='$4' passwo
 ogr2ogr -a_srs EPSG:2154 $1 PG:"host='$2' port='$3' user='$6' dbname='$4' password='$7' schemas=$5" france_cassini_roads
 
 ogr2ogr -f CSV $1/version.csv PG:"host='$2' port='$3' user='$6' dbname='$4' password='$7' schemas=$5" -sql "SELECT last_event, tolerance, tolerance_cities FROM version"
+
+tables=(node edge face edge_duplicate france_cassini_cities france_cassini_roads)
+
+for item in ${tables[*]}
+do
+  printf "   %s\n" $item
+  tar cvjf $1/$item.tgz $1/$item.shp $1/$item.dbf $1/$item.shx $1/$item.prj $1/version.csv
+  rm $1/$item.shp $1/$item.dbf $1/$item.shx $1/$item.prj
+done
+
